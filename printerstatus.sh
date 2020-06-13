168a640baa15486a06d15e5c6b9a68a3f3f8bdba
beginner3456789 <commander1oldsmobile@yahoo.com>
2020-06-13 10:52:02 -0700

    Add files via upload

---
+++ printerstatus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this bash script get the printer status
+# bash needs --enable-net-redirections build option
+
+# change printer IP address as needed
+
+exec 5<>/dev/tcp/192.168.1.111/9100 || echo "Printer not found."; exit 1
+
+echo -e "\033%-12345X@PJL" >&5
+echo @PJL INFO STATUS      >&5
+echo -e "\033%-12345X"     >&5
+
+( trap exit SIGINT; cat <&5 )      # Cntl-C when finished
+
+exec 5>&-
+exec 5<&-
+echo Finished reading printer.
+exit 0
